77eb299f590d37fa2242f87cd66032baa91b4286
RuslanGon <ruslan441006@gmail.com>
2024-07-09 20:55:05 +0300

    wer

--- src/components/BoxForm/BoxForm.jsx
+++ src/components/BoxForm/BoxForm.jsx
@@ -1,7 +1,9 @@
 
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup'; 
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup'; // Необходимо для валидации, хотя react-hook-form не требует использования Yup
 import css from '../BoxForm/BoxForm.module.css';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 
 const validationSchema = Yup.object().shape({
   userEmail: Yup.string().email('Invalid email').required('Required'),
@@ -9,55 +11,49 @@ const validationSchema = Yup.object().shape({
 });
 
 const BoxForm = () => {
-  const handleSubmit = (values, { resetForm }) => {
-    console.log('Email:', values.userEmail);
-    console.log('Password:', values.password);
-    resetForm();
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(validationSchema) // Используем Yup для валидации с помощью resolver
+  });
+
+  const onSubmit = (data) => {
+    console.log('Email:', data.userEmail);
+    console.log('Password:', data.password);
+    // Ваши дополнительные действия после отправки формы
   };
 
   return (
     <div className={css.div}>
       <h1 className={css.maintitle}>AquaTrack</h1>
-      <Formik
-        initialValues={{ userEmail: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ errors, touched }) => (
-          <Form className={css.boxForm}>
-            <h2 className={css.title}>Sign In</h2>
-            <div className={css.label}>
-              <label htmlFor="userEmail" className={css.labelText}>Email</label>
-              <Field
-                type="email"
-                id="userEmail"
-                name="userEmail"
-                placeholder="Enter your email"
-                className={css.input}
-              />
-              {errors.userEmail && touched.userEmail && (
-                <div className="error">{errors.userEmail}</div>
-              )}
-            </div>
-            <div className={css.label}>
-              <label htmlFor="password" className={css.labelText}>Password</label>
-              <Field
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter your password"
-                className={css.input}
-              />
-              {errors.password && touched.password && (
-                <div className="error">{errors.password}</div>
-              )}
-            </div>
-            <button type="submit" className={css.button}>
-              Sign In
-            </button>
-          </Form>
-        )}
-      </Formik>
+      <form onSubmit={handleSubmit(onSubmit)} className={css.boxForm}>
+        <h2 className={css.title}>Sign In</h2>
+        <div className={css.label}>
+          <label htmlFor="userEmail" className={css.labelText}>Email</label>
+          <input
+            type="email"
+            id="userEmail"
+            name="userEmail"
+            placeholder="Enter your email"
+            className={css.input}
+            {...register('userEmail')}
+          />
+          {errors.userEmail && <div className="error">{errors.userEmail.message}</div>}
+        </div>
+        <div className={css.label}>
+          <label htmlFor="password" className={css.labelText}>Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            className={css.input}
+            {...register('password')}
+          />
+          {errors.password && <div className="error">{errors.password.message}</div>}
+        </div>
+        <button type="submit" className={css.button}>
+          Sign In
+        </button>
+      </form>
       <p>
         Don’t have an account? <a href="">Sign Up</a>
       </p>
