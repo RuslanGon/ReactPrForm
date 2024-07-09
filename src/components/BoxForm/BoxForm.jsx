e27ff42113619d2b1096dd13db2dae8dea566956
RuslanGon <ruslan441006@gmail.com>
2024-07-09 17:51:10 +0300

    wer

--- src/components/BoxForm/BoxForm.jsx
+++ src/components/BoxForm/BoxForm.jsx
@@ -1,25 +1,21 @@
-import css from '../BoxForm/BoxForm.module.css'
 
+import css from '../BoxForm/BoxForm.module.css';
 
 const BoxForm = () => {
   return (
-    <form>
-        <h2>Sign In</h2>
-        <label>
-            <span>Email</span>
-           
-            <input type="email" name="userEmail" placeholder="Enter your email" />
-        </label>
-       
-        <label>
-            <span>Password</span>
-           
-            <input type="text" name="password" placeholder="Enter your password" />
-        </label>
-        
-        <button type="button">Sign In</button>
+    <form className={css.boxForm}>
+      <h2 className={css.title}>Sign In</h2>
+      <label className={css.label}>
+        <span className={css.labelText}>Email</span>
+        <input type="email" name="userEmail" placeholder="Enter your email" className={css.input} />
+      </label>
+      <label className={css.label}>
+        <span className={css.labelText}>Password</span>
+        <input type="text" name="password" placeholder="Enter your password" className={css.input} />
+      </label>
+      <button type="button" className={css.button}>Sign In</button>
     </form>
-  )
+  );
 }
 
-export default BoxForm
+export default BoxForm;
