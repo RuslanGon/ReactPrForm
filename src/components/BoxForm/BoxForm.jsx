4329e74a49e76c2daf93838ba91c36d52954d9db
RuslanGon <ruslan441006@gmail.com>
2024-07-09 17:38:21 +0300

    wer

---
+++ src/components/BoxForm/BoxForm.jsx
@@ -0,0 +1,19 @@
+
+const BoxForm = () => {
+  return (
+    <form>
+        <h2>Sign In</h2>
+        <label>
+            <span>Email</span>
+            <br />
+            <input type="email" name="userEmail" placeholder="Enter your email" />
+        </label>
+        <label>
+            <span>Password</span>
+            <input type="text" name="password" placeholder="Enter your password" />
+        </label>
+    </form>
+  )
+}
+
+export default BoxForm
