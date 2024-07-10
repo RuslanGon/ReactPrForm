7e839de83b02011b130e23d473a7c07f7fe23c50
RuslanGon <ruslan441006@gmail.com>
2024-07-10 12:45:23 +0300

    wer

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,3 +1,6 @@
+import css from '../../components/Counter/Counter.module.css';
+import plus from '../../image/plus.png'
+import plus1 from '../../image/plus (1).png'
 
 
 const  Counter = ({handleCounter}) => {
@@ -6,8 +9,9 @@ const  Counter = ({handleCounter}) => {
         <h1>Edit the entered amount of water</h1>
         <h3>Correct entered data:</h3>
         <p>Amount of water:</p>
-        
+        <button className={css.buttonCounter}><img src={plus} alt="" /></button>
         <button onClick={handleCounter}>hello</button>
+        <button className={css.buttonCounter}><img src={plus1} alt="" /></button>
     </div>
   )
 }
