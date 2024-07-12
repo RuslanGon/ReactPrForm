16d00dfae184c3f6c44a3f3f5633d402096ee737
RuslanGon <ruslan441006@gmail.com>
2024-07-12 15:36:19 +0300

    wer

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,8 +2,14 @@
 import css from '../Modal/Modal.module.css';
 
 const Modal = ({ show, handleClose, children }) => {
+  const handleBackgroundClick = (e) => {
+    if (e.target === e.currentTarget) {
+      handleClose();
+    }
+  };
+
   return (
-    <div className={`${css.modal} ${show ? css.show : ''}`}>
+    <div className={`${css.modal} ${show ? css.show : ''}`} onClick={handleBackgroundClick}>
       <div className={css.modalContent}>
         <span className={css.close} onClick={handleClose}>&times;</span>
         {children}
@@ -13,3 +19,4 @@ const Modal = ({ show, handleClose, children }) => {
 };
 
 export default Modal;
+
