6be66d50e0a52f9ced2a516c17b60dbac0a76234
RuslanGon <ruslan441006@gmail.com>
2024-07-14 15:14:54 +0300

    wer

--- src/components/Batle/Batle.jsx
+++ src/components/Batle/Batle.jsx
@@ -1,20 +1,31 @@
-import css from '../Batle/Batle.module.css'
-import foto from '../../image/foto.png'
-import vector1 from '../../image/vector1.png'
+import css from "../Batle/Batle.module.css";
+import foto from "../../image/foto.png";
+import vector1 from "../../image/vector1.png";
+import plusik from '../../image/plusik.png'
 const Batle = () => {
   return (
     <div className={css.divmain}>
-        <div className={css.header}>
-        <h1 className={css.title1}>Hello, <span className={css.spantitle}>Nadia!</span></h1>
+      <div className={css.header}>
+        <h1 className={css.title1}>
+          Hello, <span className={css.spantitle}>Nadia!</span>
+        </h1>
         <div className={css.divleb}>
-        <h4 className={css.title}>Nadia</h4>
-        <img className={css.foto} src={foto} alt="" />
-        <button className={css.btn}><img src={vector1} alt="" /></button>
-        
-    </div>
+          <h4 className={css.title}>Nadia</h4>
+          <img className={css.foto} src={foto} alt="" />
+          <button className={css.btn}>
+            <img src={vector1} alt="" />
+          </button>
         </div>
-    </div>
-  )
-}
+      </div>
+      <div>
+      <h2>Today</h2>
+      <button> <img src={plusik} alt="" /></button>
+      <p>Add water</p>
+     
 
-export default Batle
+      </div>
+    </div>
+  );
+};
+
+export default Batle;
