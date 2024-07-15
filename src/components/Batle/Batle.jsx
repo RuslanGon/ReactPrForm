17f2f47bbf3a275dfd044316e1078c81d2839c94
RuslanGon <ruslan441006@gmail.com>
2024-07-15 11:07:11 +0300

    wer

--- src/components/Batle/Batle.jsx
+++ src/components/Batle/Batle.jsx
@@ -16,65 +16,50 @@ const Batle = () => {
 
   return (
     <div className={css.divmain}>
-      <div className={css.header}>
-        <h1 className={css.title1}>
-          Hello, <span className={css.spantitle}>Nadia!</span>
-        </h1>
-        <div className={css.divleb}>
-          <h4 className={css.title}>Nadia</h4>
-          <img className={css.foto} src={foto} alt="" />
-          <button className={css.btn}>
-            <img src={vector1} alt="" />
-          </button>
-        </div>
-      </div>
-      <div className={css.head}>
-        <h2 className={css.tet}>Today</h2>
+    <div className={css.header}>
+      <h1 className={css.title1}>
+        Hello, <span className={css.spantitle}>Nadia!</span>
+      </h1>
+      <div className={css.divleb}>
+        <h4 className={css.title}>Nadia</h4>
+        <img className={css.foto} src={foto} alt="Profile" />
         <button className={css.btn}>
-          <img src={plusik} alt="" />
+          <img src={vector1} alt="Menu" />
         </button>
-        <p className={css.add}>Add water</p>
       </div>
-      <ul className={css.list}>
-        <li className={css.item}>
-          <img src={water} width={44} height={45} alt="" />
+    </div>
+    <div className={css.head}>
+      <h2 className={css.tet}>Today</h2>
+      <button className={css.btn}>
+        <img src={plusik} alt="Add" />
+      </button>
+      <p className={css.add}>Add water</p>
+    </div>
+    <ul className={css.list}>
+      {[
+        { time: '7:00 AM', volume: '250 ml' },
+        { time: '11:00 AM', volume: '250 ml' },
+        { time: '14:00 PM', volume: '250 ml' }
+      ].map((item, index) => (
+        <li className={css.item} key={index}>
+          <img src={water} width={44} height={45} alt="Water" />
           <div className={css.div7}>
-            <span className={css.spamli}>250 ml</span>
-            <span className={css.spamlii}>7:00 AM</span>
+            <span className={css.spamli}>{item.volume}</span>
+            <span className={css.spamlii}>{item.time}</span>
           </div>
           <div className={css.div6}>
-            <img src={pan} width={16} height={16} alt="" />
-            <img src={kor} width={16} height={16} alt="" />
+            <img src={pan} width={16} height={16} alt="Edit" />
+            <img src={kor} width={16} height={16} alt="Delete" />
           </div>
         </li>
-        <li className={css.item}>
-          <img src={water} width={44} height={45} alt="" />
-          <div className={css.div7}>
-            <span className={css.spamli}>250 ml</span>
-            <span className={css.spamlii}>11:00 AM</span>
-          </div>
-          <div className={css.div6}>
-            <img src={pan} width={16} height={16} alt="" />
-            <img src={kor} width={16} height={16} alt="" />
-          </div>
-        </li>
-        <li className={css.item}>
-          <img src={water} width={44} height={45} alt="" />
-          <div className={css.div7}>
-            <span className={css.spamli}>250 ml</span>
-            <span className={css.spamlii}>14:00 AM</span>
-          </div>
-          <div className={css.div6}>
-            <img src={pan} width={16} height={16} alt="" />
-            <img src={kor} width={16} height={16} alt="" />
-          </div>
-        </li>
-      </ul>
-      <hr className={css.line} />
-      <div className={css.customCalendar}>
-        <div className={css.calendarHeader}>
-          <div className={css.divmoth1}>
-            <h2 className={css.stata}>Statistics</h2>
+      ))}
+    </ul>
+    <hr className={css.line} />
+    <div className={css.customCalendar}>
+      <div className={css.calendarHeader}>
+        <div className={css.divmoth1}>
+          <h2 className={css.stata}>Statistics</h2>
+          <div className={css.monthWrapper}>
             <button
               className={css.button}
               onClick={() =>
@@ -85,7 +70,7 @@ const Batle = () => {
             >
               <img src={left} alt="Previous month" />
             </button>
-            <span>
+            <span className={css.monthText}>
               {currentDate.toLocaleString("en-US", { month: "long" })},{" "}
               {currentDate.getFullYear()}
             </span>
@@ -100,11 +85,12 @@ const Batle = () => {
               <img src={right} alt="Next month" />
             </button>
           </div>
-          <img className={css.pie} src={pie} alt="Pie chart" />
-          
         </div>
+        <img className={css.pie} src={pie} alt="Pie chart" />
       </div>
     </div>
+  </div>
+  
   );
 };
 
